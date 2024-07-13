6ae71dc33f374c4500012b9c3eb237064b7c01ab
tzafff <christzaf99@gmail.com>
2024-07-13 19:26:55 +0300

    update url

--- dexBack/index.js
+++ dexBack/index.js
@@ -1,39 +1,48 @@
 const express = require("express");
 const Moralis = require("moralis").default;
-const app = express();
 const cors = require("cors");
 require("dotenv").config();
-const port = 3001;
 
-app.use(cors());
+const app = express();
+const port = process.env.PORT || 3001;
+
+// Use CORS middleware to handle CORS issues
+app.use(cors({
+  origin: 'https://dex-web3.vercel.app', // Your frontend URL
+  optionsSuccessStatus: 200
+}));
+
 app.use(express.json());
 
 app.get("/tokenPrice", async (req, res) => {
+  const { query } = req;
 
-  const {query} = req;
+  try {
+    const responseOne = await Moralis.EvmApi.token.getTokenPrice({
+      address: query.addressOne,
+    });
 
-  const responseOne = await Moralis.EvmApi.token.getTokenPrice({
-    address: query.addressOne
-  })
+    const responseTwo = await Moralis.EvmApi.token.getTokenPrice({
+      address: query.addressTwo,
+    });
 
-  const responseTwo = await Moralis.EvmApi.token.getTokenPrice({
-    address: query.addressTwo
-  })
+    const usdPrices = {
+      tokenOne: responseOne.raw.usdPrice,
+      tokenTwo: responseTwo.raw.usdPrice,
+      ratio: responseOne.raw.usdPrice / responseTwo.raw.usdPrice,
+    };
 
-  const usdPrices = {
-    tokenOne: responseOne.raw.usdPrice,
-    tokenTwo: responseTwo.raw.usdPrice,
-    ratio: responseOne.raw.usdPrice/responseTwo.raw.usdPrice,
+    return res.status(200).json({ usdPrices });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "An error occurred while fetching token prices." });
   }
-
-
-  return res.status(200).json({usdPrices});
 });
 
 Moralis.start({
   apiKey: process.env.MORALIS_KEY,
 }).then(() => {
   app.listen(port, () => {
-    console.log(`Listening for API Calls`);
+    console.log(`Listening for API Calls on port ${port}`);
   });
 });
